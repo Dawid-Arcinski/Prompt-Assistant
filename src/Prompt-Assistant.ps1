418614310fdcd8952666b3f50265e720798da2c4
Dawid-Arcinski <dawid.arcinski@protonmail.com>
2024-09-18 20:55:16 +0200

    Added script files.

---
+++ src/Prompt-Assistant.ps1
@@ -0,0 +1,40 @@
+param(
+    [switch]$help,
+    [string]$template
+)
+
+# STARTUP
+
+$mainDir = [System.Environment]::GetEnvironmentVariable("PROMPT_ASSISTANT_HOME", "User")
+Set-Location $mainDir
+$promptData = Import-Csv 'data/prompts.csv'
+$templates = $promptData.template
+
+# VALIDATION
+
+if ($help) {
+    $promptData | Format-Table -Wrap
+    Exit
+}
+
+if ($template -and ($templates -notcontains $template)) {
+    Write-Error "Template $template is not present in prompt data"
+    Exit
+}
+
+# PROCESSING
+
+foreach ($entry in $promptData) {
+    if ($entry.template -eq $template) {
+        $text = $entry.text
+    }
+}
+
+$context = Get-Clipboard
+$prompt = $text + ("`n" * 2) + $context
+Set-Clipboard $prompt
+
+
+#test
+
+# Write-Host $templates
